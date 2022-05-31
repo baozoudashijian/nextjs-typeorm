6a8a1018fc40554c0de0489d22c0601217eb937c
Roger Zhang <1787009521@qq.com>
2022-05-31 12:22:28 +0800

    feat: 生成migration

---
+++ src/migration/1653970905511-CreatePost.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreatePost1653970905511 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
